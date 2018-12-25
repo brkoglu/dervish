7f63ac813e273784614d0f5671fafc8cee1c2925
brkoglu <burak.hueseyinoglu@gmail.com>
2018-12-25 01:03:11 +0100

    Init

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+//webpack.config.js
+var path = require('path');
+var webpack = require('webpack');
+module.exports = {
+ entry: './client/index.js',
+ output: {
+  path: path.join(__dirname, 'client'),
+  filename: 'bundle.js'
+ },
+ resolve: {
+    extensions: ['.webpack.js', '.web.js', '.ts', '.js']
+    },
+ module: {
+  loaders: [{
+   test: /.jsx?$/,
+   loader: 'babel-loader',
+   exclude: /node_modules/,
+   query: {
+    presets: ['es2015', 'react']
+   }
+  },
+  {
+   test: /\.css$/,
+   loader: "style-loader!css-loader"
+  }]
+ }
+};
